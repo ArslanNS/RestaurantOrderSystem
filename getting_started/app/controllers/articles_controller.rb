c2e3a1c8da2b92aee125e1db1161297861282314
Preshant Achuthan <engerium@gmail.com>
2020-01-23 23:59:44 +0000

    Added form to new view + added create action for Articles controller

--- getting_started/app/controllers/articles_controller.rb
+++ getting_started/app/controllers/articles_controller.rb
@@ -1,5 +1,22 @@
 class ArticlesController < ApplicationController
-  def new
-
+  def show
+    @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new()
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    @article.save
+
+    redirect_to @article
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text);
+    end
 end
