623490754b45adb31a3af03fe56f0229b3d63f26
Mathias Helseth <zfac134@live.rhul.ac.uk>
2020-01-24 01:59:11 +0000

    Added edit functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,38 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    @articles = Article.all.order("created_at DESC")
   end
 
   def new
+    @article = Article.new
   end
 
   def show
     @article = Article.find(params[:id])
   end
 
-  def create
-    @article = Article.new(params.require(:article).permit(:title, :text))
+  def edit
+    @article = Article.find(params[:id])
+  end
 
-    @article.save
-    redirect_to @article
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   private
