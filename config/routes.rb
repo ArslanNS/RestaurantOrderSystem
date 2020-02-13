52aee83d149a3a6d688127ffd2c849a30a67a9d8
Preshant Achuthan <engerium@gmail.com>
2020-02-13 19:20:01 +0000

    Fixed paths in foods controller and views + updated food controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root 'welcome#index'
+
   resources :menu, only: [:index, :create]
-  get 'dashboard/index'
+  resources :dashboard, only: [:index]
+
+  namespace :dashboard do
+    resources :foods, controller: '/foods'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
