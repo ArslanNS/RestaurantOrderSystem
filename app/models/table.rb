ce6f748a29ce70480608351d5e1cc987460391d0
Preshant Achuthan <engerium@gmail.com>
2020-02-25 00:46:22 +0000

    Added Table association to User

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,3 +1,4 @@
 class Table < ApplicationRecord
   has_many :calls
+  has_one :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_234547) do
+ActiveRecord::Schema.define(version: 2020_02_25_003931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,8 +75,10 @@ ActiveRecord::Schema.define(version: 2020_02_24_234547) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "table_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["table_id"], name: "index_users_on_table_id"
   end
 
   create_table "users_roles", id: false, force: :cascade do |t|
@@ -89,4 +91,5 @@ ActiveRecord::Schema.define(version: 2020_02_24_234547) do
 
   add_foreign_key "calls", "tables"
   add_foreign_key "foods", "sections"
+  add_foreign_key "users", "tables"
 end
