d5fc6d9fc314a89b589625186c6d96f61de6df27
Milandeep Bassi <milandeep.bassi.2018@live.rhul.ac.uk>
2020-02-28 07:28:48 +0000

    Setup controller for Calls to be accessed in index.html.erb

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,21 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
 
+  # GET /foods
+  # GET /foods.json
   def index
+    @call = Call.all
   end
+
+  # GET /foods/1
+  # GET /foods/1.json
+  def show
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_food
+    @call = Call.find(params[:id])
+  end
+
 end
