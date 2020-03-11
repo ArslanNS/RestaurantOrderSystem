8a455f7154e36562455ecff9c946f4aec99de4e7
Milandeep Bassi <milandeep.bassi.2018@live.rhul.ac.uk>
2020-03-11 20:18:38 +0000

    Added route for the new card display to the routes.rb file.

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   end
 
   resources :billing, only: [:index]
+  get '/card/new' => 'billing#new_card', as: :add_payment_method
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
