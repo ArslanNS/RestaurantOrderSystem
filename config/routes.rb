1cb1e098cf3da074e65c4cf54698ecb61f2ba4d0
Milandeep Bassi <milandeep.bassi.2018@live.rhul.ac.uk>
2020-03-12 23:35:39 +0000

    Completed adding full Stripe payment support.

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
   end
 
   get '/card/new' => 'billing#new_card', as: :add_payment_method, xhr:true
+  post "/card" => "billing#create_card", as: :create_payment_method
+  get '/success' => 'billing#success', as: :success
+  post '/subscription' => 'billing#subscribe', as: :subscribe
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
