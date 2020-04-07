8fd7b5a57a8fac6a8ce67ceb5d681a220a329c9a
farhadk3 <zfac224@live.rhul.ac.uk>
2020-04-07 20:02:45 +0100

    updated methods

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,13 +6,31 @@ class DashboardController < ApplicationController
   def index
     @calls = Call.all
     @orders = Order.all
+
   end
+
+
 
   # GET /foods/1
   # GET /foods/1.json
   def show
   end
 
+  def numbering(table_number)
+    @waiter_table = table_number
+    if @waiter_table == table_number
+
+    end
+    puts "AHAHAHHAHAHHA"
+    puts @waiter_table.inspect
+    puts "AHAHAHHAHAHHA"
+
+
+
+
+  end
+
+  helper_method :numbering
 
 
   def confirm(order)
