8b384fc6ce6e6922a4fa9cc851ec031b99787d12
zfac138 <zfac138@live.rhul.ac.uk>
2020-04-09 19:09:03 +0100

    Redirect user sign in to dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # def after_sign_in_path_for(resource)
-  #   dashboard_path(current_user) #your path
-  # end
+  def after_sign_in_path_for(resource)
+    dashboard_index_url #your path
+  end
 end
