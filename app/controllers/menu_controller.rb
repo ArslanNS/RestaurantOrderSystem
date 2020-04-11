0f3143f87f2f6236281a3d401ba2023eb0729803
Preshant Achuthan <engerium@gmail.com>
2020-04-12 00:07:08 +0800

    Added comments

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,13 +1,17 @@
 class MenuController < ApplicationController
   def index
+
+    # Get objects for display on views
     @call = Call.new
     @foods = Food.all
     @sections = Section.all
     @order = Order.new
 
+    # Get table and bill based on current user
     current_table = current_user.table
     current_bill_id = current_table.current_bill_id
 
+    # Get current bill object for display on views
     @current_bill = Bill.find(current_bill_id)
 
   end
